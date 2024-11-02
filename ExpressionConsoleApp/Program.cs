1c375dc758b635d1f87b330c4aa67867d064b1ac
BenPostlethwaite <98238741+BenPostlethwaite@users.noreply.github.com>
2024-11-02 22:50:09 +0000

    Add files via upload

---
+++ ExpressionConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using MathsLibrary;
+
+class Program
+{
+    static void Main()
+    {
+        Expression expression = new Expression("x^(1+1/y^2)", "infix");
+        expression.ExpandAndSimplify();
+        expression.MakePretty();
+        Console.WriteLine(expression.ToString());
+    }
+}
